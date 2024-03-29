a2548fc4c1082722771de7582addb4c669148842
divingbeetle <radi_ohead@yonsei.ac.kr>
2024-03-29 18:04:48 +0900

    Fix typo

--- components/prism-cpp.js
+++ components/prism-cpp.js
@@ -21,10 +21,10 @@
 			// This also intends to capture the class name of method implementations but here the class has template
 			// parameters, so it can't be a namespace (until C++ adds generic namespaces).
 			/\b\w+(?=\s*<(?:[^<>]|<(?:[^<>]|<[^<>]*>)*>)*>\s*::\s*\w+\s*\()/,
-			// Primitive Types 
-			/\b(?:unsigned|singed|long|short|void|bool|char|int|float|double|wchar_t|auto|)\b/,
-			/\b(?:size_t|char8_t|char16_t|char32_t|int8_t|int16_t|int32_t|int64_t|uint8_t|uint16_t|uint32_t|uint64_t)\b/
-			// Common library Types 
+			// Primitive Types
+			/\b(auto|bool|char|double|float|int|long|short|singed|unsigned|void|wchar_t)\b/,
+			/\b(char16_t|char32_t|char8_t|int16_t|int32_t|int64_t|int8_t|size_t|uint16_t|uint32_t|uint64_t|uint8_t)\b/
+			// Common library Types
 		],
 		'keyword': keyword,
 		'number': {
@@ -32,11 +32,12 @@
 			greedy: true
 		},
 		'operator': />>=?|<<=?|->|--|\+\+|&&|\|\||[?:~]|<=>|[-+*/%&|^!=<>]=?|\b(?:and|and_eq|bitand|bitor|not|not_eq|or|or_eq|xor|xor_eq)\b/,
-		'boolean': /\b(?:false|true)\b/
+		'boolean': /\b(?:false|true)\b/,
 		'variable': [
 			{
 				// struct members
-				pattern: /(?:\.|->)\s*[a-zA-Z_]\w*/
+				pattern: /(?:\.|->)\s*[a-zA-Z_]\w*/,
+				lookbehind: true
 			}
 		]
 	});
